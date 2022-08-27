44d9cb6f3c0ddc18fe39b4e5acdcbb6756db4e4d
tomoroltuto <to1mo2.to2mo3@gmail.com>
2022-08-28 00:03:18 +0900

    first commit

---
+++ src/main/java/com/example/JavaChallenge7/JavaChallenge7Application.java
@@ -0,0 +1,13 @@
+package com.example.JavaChallenge7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaChallenge7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaChallenge7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JavaChallenge7/JavaChallenge7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaChallenge7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaChallenge7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
